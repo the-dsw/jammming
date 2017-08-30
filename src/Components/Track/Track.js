3a447bb25d0f7f851a660f4a2642719cbb6ab254
the-dsw <fmod@the-dsw.com>
2017-08-30 22:36:45 +0200

    add anchor tag to track list

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,22 +2,32 @@ import React, { Component } from 'react';
 import './Track.css';
 
 class Track extends Component {
-
-    renderAction() {
-
+   constructor(props) {
+        super(props);
+        this.state = {
+            tag: "+"
+        }
+        this.renderAction = this.renderAction.bind(this);
+   }
+    renderAction(e) {
+        e.preventDefault();
+        let tag;
+        this.state.tag === '+' ? tag = this.setState({ tag: '-'}) : tag = this.setState({ tag: '+'});  
+                      
+        return tag;
     }
     
-  render() {
-    return (
-        <div className="Track">
-            <div className="Track-information">
-                <h3>{this.props.track.name}</h3>
-                <p>{this.props.track.artist} | {this.props.track.album}</p>
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
+                </div>
+                <a className="Track-action" onClick={this.renderAction}>{this.state.tag}</a>
             </div>
-            <a className="Track-action">+{/*<!-- + or - will go here -->*/}</a>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default Track;
